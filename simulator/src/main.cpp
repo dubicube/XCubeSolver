e08e73f4f9f5731a22a214216b5a650da9f4ed04
dubicube <49446589+dubicube@users.noreply.github.com>
2020-04-09 18:28:57 +0200

    Initiated project with hello world

---
+++ simulator/src/main.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+int main(int argc, char* argv[]) {
+    cout << "Coucou" << endl;
+}
